234137e79f84e8914355e7a654dcf0cdf83c74e6
fadzreen43 <fadzreen34@gmail.com>
2020-02-23 03:35:47 +0800

    Scroll View

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  const[name,setName] = useState('shaun');
-  const[age,setAge] = useState(40);
+ const [people, setPeople] = useState([
+   {name: 'shaun', key: '1'},
+   {name: 'yoshi', key: '2'},
+   {name:'mario', key: '3'},
+   {name: 'luigi', key: '4'},
+   {name: 'peach', key:'5'},
+   {name:'toad', key:'6'},
+   {name:'bowser', key:'7'},
+ ]);
 
+ return(
+   <View style={styles.container}>
 
-  return (
-    <View style={styles.container}>
-      
-      {/* START INPUT SECTION */}
-      <Text>Enter name:</Text>
-      <TextInput 
-        multiline
-        style={styles.input}  
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)}/>
-
-      <Text>Enter age:</Text>
-      <TextInput 
-      keyboardType='numeric'
-        style={styles.input}  
-        placeholder='e.g. 99'
-        onChangeText={(val) => setAge(val)}/>
-      {/* END INPUT SECTION */}
-
-      {/* OUTPUT SECTION */}
-        <Text>name: {name} , age: {age}</Text>
-    </View>
-  );
+     {/* This section told how to use scroll view */}
+     
+     <ScrollView>
+    {people.map((item)=>{
+      return(
+        <View key={item.key}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>  
+      )
+    })}
+    </ScrollView>
+   </View>
+ )
 }
 
 // STYLE (CSS SECTION)
@@ -36,14 +35,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input:{
-    borderWidth:1,
-    borderColor:'#777',
-    padding:8,
-    margin:10,
-    width:200
+  item: {
+    marginTop: 24,
+    padding:30,
+    backgroundColor: 'pink',
+    fontSize: 24
   }
 });
